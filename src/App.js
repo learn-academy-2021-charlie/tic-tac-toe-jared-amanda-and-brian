ab78c8a18bea11b6794aa1d93c273be138de288d
Jatded1d9l7 <jthoma25wv@gmail.com>
2021-07-27 14:49:11 -0400

    working I should be able to click on a square to mark it.

--- src/App.js
+++ src/App.js
@@ -6,15 +6,37 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      player1: "x"
     }
   }
+
+handleGamePlay = (index) => {
+  const {squares} = this.state
+  if ( index === player1){
+  squares[index] = "x"
+  this.setState({squares: squares})
+}else {
+  squares[index] = "o"
+  this.setState({squares: squares})
+}}
 
   render(){
     return(
       <>
         <h1>Tic Tac Toe</h1>
-        <Square />
+        <div id = "gameboard" >
+        {this.state.squares.map((value, index) => {
+          return (
+          <Square
+          value= {value}
+          key= {index}
+          index={index}
+          handleGamePlay= {this.handleGamePlay}
+          />
+        )
+        })}
+          </div>
       </>
     )
   }
